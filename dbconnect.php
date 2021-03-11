12fd12012ed26506ed836970f6f5d455a74885ac
aditya-aditi <73513143+aditya-aditi@users.noreply.github.com>
2021-03-11 11:40:47 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,10 @@
 <?php
 
 // Script to connect to the database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "phpchat";
+$servername = "ec2-54-145-102-149.compute-1.amazonaws.com";
+$username = "scfhoeqdouujge";
+$password = "d9f946a6d77dcc094d550f39bf7070ce3e98454a80d96ab18caa0ed095afc40f";
+$database = "dfjv7vjr83sm8o";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
